6ce5d46e804a45a960a545c4a80b7cec039d31b6
i5x64BIT <vaintomer@gmail.com>
2023-08-03 12:44:39 +0300

    Add book addition and removal via Map
    Book addition and removal is now done via map instead of an array

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const arrBook = [];
+const bookMap = new Map();
 function Book(title, author, pages, description) {
+    this.id = ++Book.id;
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -10,10 +11,48 @@ function Book(title, author, pages, description) {
         return JSON.stringify(this);
     }
 }
+Book.id = 0; //Static variable
 
 const btnAdd = document.querySelector(".content-container .card:last-child");
 const formContainer = document.querySelector('.form-container');
 const form = document.querySelector('.form-container form')
+
+function createBookCard(id, title, author, pages, description){
+    const card = document.createElement('div');
+    card.classList = 'card';
+
+    const bTitle = document.createElement('h3');
+    bTitle.classList = 'book-info';
+    bTitle.innerText = title;
+
+    const bPages = document.createElement('p');
+    bPages.classList = 'book-info';
+    bPages.innerText = `${pages} Pages`;
+
+    const bAuthor = document.createElement('p');
+    bAuthor.classList = 'book-info';
+    bAuthor.innerText = author;
+
+    const bDescription = document.createElement('p');
+    bDescription.classList = 'book-description text-small';
+    bDescription.innerText = description;
+
+    [bTitle, bPages, bAuthor, bDescription].forEach(e => card.appendChild(e));
+    card.innerHTML += '<div class="book-controls"><button class="btn btn-danger">Remove</button></div>'; //Add the remove btn
+
+    card.setAttribute('data-id', id);
+
+    const btnRemoveBook = card.querySelector(`.card[data-id="${id}"] .btn-danger`)
+    btnRemoveBook.addEventListener('click', function() {
+        const bookCard = this.parentElement.parentElement;
+        const bookId = this.parentElement.parentElement.getAttribute('data-id');
+
+        removeBookFromMap(id);
+        document.querySelector('.content-container').removeChild(bookCard);
+    })
+    
+    return card;
+}
 
 btnAdd.addEventListener('click', () => {
     formContainer.style.display = 'flex';
@@ -26,8 +65,11 @@ form.addEventListener('mouseenter', () => {
     formContainer.removeEventListener('click', hideElem);
 })
 
-const addBookToLibrary = (book) =>{
-    arrBook.push(book);
+const addBookToMap = (book) =>{
+    bookMap.set(book.id, book);
+}
+const removeBookFromMap = (id) => {
+    bookMap.delete(id)
 }
 const setIsRead = (iBook, isRead) => {
     arrBook[iBook].isRead = isRead;
@@ -38,41 +80,29 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
 })
 btnSubmitBook.addEventListener('click', () => {
-    function createBookCard(title, author, pages, description){
-        const card = document.createElement('div');
-        card.classList = 'card';
-
-        const bTitle = document.createElement('h3');
-        bTitle.classList = 'book-info';
-        bTitle.innerText = title;
-
-        const bPages = document.createElement('p');
-        bPages.classList = 'book-info';
-        bPages.innerText = `${pages} Pages`;
-
-        const bAuthor = document.createElement('p');
-        bAuthor.classList = 'book-info';
-        bAuthor.innerText = author;
-
-        const bDescription = document.createElement('p');
-        bDescription.classList = 'book-description text-small';
-        bDescription.innerText = description;
-
-        [bTitle, bPages, bAuthor, bDescription].forEach(e => card.appendChild(e));
-        card.innerHTML += '<div class="book-controls"><button class="btn btn-danger">Remove</button></div>'; //Add the remove btn
-        return card;
-    }
-    addBookToLibrary( new Book(
+    const book = new Book(
         document.querySelector('.form-container input[name="title"]').value,
         document.querySelector('.form-container input[name="author"]').value,
         document.querySelector('.form-container input[name="pages"]').value,
         document.querySelector('.form-container textarea[name="description"]').value
-    ));
+    );
+    addBookToMap(book);
     //Add the book to the ui
-    const book = JSON.parse( arrBook[arrBook.length-1].info() );
-    const card = createBookCard(book.title, book.author, book.pages, book.description);
+    const card = createBookCard(book.id ,book.title, book.author, book.pages, book.description);
     
     document.querySelector('.content-container')
     .insertBefore( card ,document.querySelector('.content-container .card:last-child') );
     hideElem.call(formContainer);
 })
+
+// const btnRemoveList = document.querySelectorAll('.card .btn-danger');
+
+// btnRemoveList.forEach(b => b.addEventListener('click',  function() {
+//     console.log('Remove Element was clicked')
+    
+
+//     arrBook.splice(bookId, 1);
+//     document.querySelector('.content-container').removeChild(bookCard);
+// }));
+
+
